e07be70022fb79a19fb1c92adc6ba79ec3445340
amayas0615 <amayasmelab@gmail.com>
2025-05-10 11:00:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
+<?php
+require_once './core/Database.php';
+require_once './controllers/TaskController.php';
+$db = (new Database())->getConnection();
+$controller = new TaskController($db);
+$action = $_GET['action'] ?? 'index';
+if (method_exists($controller, $action)) {
+ $controller->$action();
+} else {
+ echo "Action non trouvée.";
+}
 
+?>
