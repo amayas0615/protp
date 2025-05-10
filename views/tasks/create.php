dfa11f555f1add78763b4fb5ec04c919bc8552a6
amayas0615 <amayasmelab@gmail.com>
2025-05-10 10:50:47 +0100

    Update create.php

---
+++ views/tasks/create.php
@@ -0,0 +1,5 @@
+<h1>Ajouter une tâche</h1>
+<form method="POST" action="index.php?action=create">
+ <input type="text" name="title" placeholder="Titre de la tâche" required>
+ <button type="submit">Ajouter</button>
+</form>
